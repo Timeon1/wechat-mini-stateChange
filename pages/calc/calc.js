a0b601d91e222788a164b65316bfa260f1f6bf69
Timeon1 <42144027+Timeon1@users.noreply.github.com>
2019-04-17 10:44:37 +0800

    init

---
+++ pages/calc/calc.js
@@ -0,0 +1,75 @@
+// pages/calc/calc.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nums: 0
+  },
+  less(){
+    this.setData({
+      nums: this.data.nums -1
+    })
+  },
+  plus(){
+    this.setData({
+      nums: this.data.nums + 1
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
