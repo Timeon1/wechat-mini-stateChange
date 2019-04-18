0b5faff2553da403a9504112f8c0483ab1aefe71
Timeon1 <42144027+Timeon1@users.noreply.github.com>
2019-04-18 09:49:46 +0800

    watch state

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-// app.js
+
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+
+  },
+    /**
+   * @brief 状态管理
+   */
+  watchCallBack: {},
+  watchingKeys: [],
+  $watch(key, cb) {
+    this.watchCallBack = Object.assign({}, this.watchCallBack, {[key]: this.watchCallBack[key] || []});
+    if (!this.watchCallBack[key]) this.watchCallBack[key] = [];
+    this.watchCallBack[key].push(cb);
+    
+    if (!this.watchingKeys.find(x => x === key)) {
+      const that = this;
+      this.watchingKeys.push(key);
+      let val = this.globalData[key];
+
+      Object.defineProperty(this.globalData, key, {
+        configurable: true,
+        enumerable: true,
+        set(value) {
+          const old = that.globalData[key];
+          val = value;
+          that.watchCallBack[key].map(func => func(value, old));
+        },
+        get() {
+          return val;
+        }
+      })
+    }
   },
   globalData: {
-    userInfo: null
+    nums: 0
   }
 })
